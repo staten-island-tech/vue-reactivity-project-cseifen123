006852fd229be5efe8ad8c75d8a9744544d04609
cseifen123 <caroline.seifen22@sitechhs.com>
2021-10-18 12:28:35 -0400

    work

--- reactivity-project/src/App.vue
+++ reactivity-project/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Subs</h1><!-- <img alt="Vue logo" :src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script> 
+// import HelloWorld from './components/HelloWorld.vue'
+var app = new Vue({
+  el: '#app',
+})
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return
   }
 }
 </script>
