02688b95ff68a94c8b5e0a56c16185c1b7257eee
cseifen123 <caroline.seifen22@sitechhs.com>
2021-10-19 12:29:11 -0400

    ideas

--- reactivity-project/src/App.vue
+++ reactivity-project/src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
-    <h1>Subs</h1><!-- <img alt="Vue logo" :src="./assets/logo.png">
+    <div class="nav-bar"></div>
+    <div class="product"></div>
+    <div class="product-info">
+      <img src="" >
+    </div>
+    <h1>Outfit of the Day</h1>
+    <p v-for="product in products" :key="product">{{product}}</p><!-- <img alt="Vue logo" :src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script> 
 // import HelloWorld from './components/HelloWorld.vue'
-
+//build your own sports jersey/ customize yiour sports jersye// slider to chansge color gradient
 export default {
   name: 'App',
   components: {
     // HelloWorld
   },
   data() {
-    return
+    return{
+      products: ["Bottoms", "Tops", "Accessories"],
+     
+    }
   }
 }
 </script>
@@ -28,4 +37,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
