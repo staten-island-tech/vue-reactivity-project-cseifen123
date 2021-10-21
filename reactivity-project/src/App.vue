e96cb96e18c46db287be2d03d8cb61b66db2a1af
cseifen123 <caroline.seifen22@sitechhs.com>
2021-10-21 12:27:26 -0400

    iterations

--- reactivity-project/src/App.vue
+++ reactivity-project/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <div class="nav-bar"></div>
-    <div class="product"></div>
-  
-    <div >
-      <img  v-bind:src="image" v-bind:alt="link" />
+    <div class="product">
+      <div class="product-image">
+        <img  v-bind:src="image" />
+      </div>
     </div>
 
-    <div class="product-info"></div>
-    <h1>ShopRight</h1>
-    <p v-for="product in products" :key="product">{{product}}</p><!-- <img alt="Vue logo" :src="./assets/logo.png">
+    <div class="product-info">
+      <h1>ShopRight</h1>
+      <p v-for="product in products" :key="product">{{product}}</p> <!--creates a div for each product-->
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div v-for="variant in variants" :key="variant">
+        <p>{{variant.variantColor}}</p>
+      </div>
+    </div>
+    <!-- <p v-if="inStock">In Stock</p>
+    <p v-else>Out of Stock</p> -->
+   
+    <!-- <span v-show="onSale">On sale!</span> -->
+<!-- <img alt="Vue logo" :src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -26,7 +36,18 @@ export default {
     return{
       products: ["Bottoms", "Tops", "Accessories"],
       image: require("./assets/grocery.jpg"),
-      link: "hello"
+      inStock: true,
+      details: ["80% cotton", "20% polyester"],
+      variants:[
+        {
+          varriantId: 2234,
+          variantColor: "green"
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue"
+        }
+      ]
     }
   }
 }
